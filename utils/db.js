b6dd5ed395f3d866b1f2d9ae7e3ce64d78e869e3
Tuva <tuva0705@gmail.com>
2025-03-08 14:19:41 +0100

    Oppdaterer database URL og legger til fallback

--- utils/db.js
+++ utils/db.js
@@ -3,12 +3,12 @@ const { Pool } = pkg;
 import dotenv from 'dotenv';
 
 dotenv.config();
+const connectionString = process.env.DATABASE_URL || "postgresql://mm207_db_user:YUleNWJXxwCJMrvWPtyLZbGJFI3oeblU@dpg-cv63jd7noe9s73bqj39g-a/mm207_db";
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // Viktig for Render
-    }
+    connectionString,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
+
 
 export default pool;
