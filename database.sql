5e5ea52ad584bffd1aca614762b312632ddd2306
Tuva <tuva0705@gmail.com>
2025-03-12 15:23:52 +0100

    oppdatert databaseoppsett lokalt og fikset ett API problem

--- database.sql
+++ database.sql
@@ -6,10 +6,3 @@ CREATE TABLE IF NOT EXISTS recipes (
     instructions TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
-
-
--- Sett inn noen eksempeldata (kan fjernes senere)
-INSERT INTO recipes (name, ingredients, instructions) VALUES
-('Super Pannekaker', ARRAY['Egg', 'Mel', 'Melk', 'Vaniljesukker'], 'Bland alt og stek i en panne.'),
-('Pizza', ARRAY['Mel', 'Vann', 'Gjær', 'Tomatsaus', 'Ost'], 'Lag deigen, ha på fyll, stek i ovnen.'),
-('Lasagne', ARRAY['Pasta', 'Kjøttdeig', 'Tomatsaus', 'Ost'], 'Bygg lagvis og stek i ovnen.');
